75cc976f16f160cb15d0aefafb920cf73f5597a2
Phantom140204 <mnithinsuresh@gmail.com>
2023-08-21 18:03:32 +0530

    Merge branch 'main' of https://github.com/Phantom140204/SAUVC24_MCU into main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #define NO_OF_THRUSTERS 7
 
+const uint8_t pinMap[NO_OF_THRUSTERS] = {PA0, PA1, PA2, PA3, PA6, PA7, PB0};
+
 void pwm_values_cb(const std_msgs::Int32MultiArray& pwm_values);
 
 Servo thrusters[NO_OF_THRUSTERS];
@@ -14,6 +16,7 @@ ros::Subscriber <std_msgs::Int32MultiArray> sub("pwm_values", &pwm_values_cb);
 void setup(){
   // Initializing the thrusters
   for (int i = 0; i <= NO_OF_THRUSTERS; i++){
+    thrusters[i].attach(pinMap[i]);
     thrusters[i].writeMicroseconds(1500);
   }
   delay(2000);
